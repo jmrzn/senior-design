2c1e8a07e299e39200bce1242c56132ededdeb83
jyoti <maharjanj191@gmail.com>
2025-10-20 17:53:45 -0700

    add text on side by side video

--- MediaPipeEx/sidebyside.py
+++ MediaPipeEx/sidebyside.py
@@ -10,10 +10,15 @@ frameWidth = int(leftVideo.get(cv2.CAP_PROP_FRAME_WIDTH))
 frameHeight = int(leftVideo.get(cv2.CAP_PROP_FRAME_HEIGHT))
 fps = int(leftVideo.get(cv2.CAP_PROP_FPS))
 
+fontX = frameWidth - 300
+fontY = frameHeight - 100
+
 # Create output video
 out = cv2.VideoWriter('MediaPipeEx/sidebyside.mp4', cv2.VideoWriter_fourcc(*"mp4v"), fps, (frameWidth * 2, frameHeight))
 
-for i in range(50):
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+while True:
     success1, frame1 = leftVideo.read()
     success2, frame2 = rightVideo.read()
 
@@ -26,6 +31,9 @@ for i in range(50):
     
     canvas[:, :frameWidth] = frame1
     canvas[:, frameWidth:] = frame2 
+
+    cv2.putText(canvas, 'ACCURACY: 100%', (fontX, fontY), font, 2, (38, 182, 103), 4, cv2.LINE_4)
+    cv2.putText(canvas, 'Good job!', (fontX, fontY+50), font, 1.5, (255, 255, 255), 2, cv2.LINE_4)
 
     out.write(canvas)
 
